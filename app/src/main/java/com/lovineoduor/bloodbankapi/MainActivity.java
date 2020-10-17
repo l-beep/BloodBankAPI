4043f4f0d31a0528ba46afb06cb40fe22c07a998
LOvine <oduor.lovine@gmail.com>
2020-10-17 23:17:16 +0300

    add bindviews

--- app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     public static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.donate) Button
+
 
 
     @Override
