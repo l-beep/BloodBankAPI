c13beac4d3380523606527396269822d6b0186bd
LOvine <oduor.lovine@gmail.com>
2020-10-17 23:42:06 +0300

    bind the codes with butterknife

--- app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     public static final String TAG = MainActivity.class.getSimpleName();
-    @BindView(R.id.donate) Button
-
-
+    @BindView(R.id.donation) Button mdonate;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.saveddonors) Button mSaveddonors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
