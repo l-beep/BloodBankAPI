a42999bccdae30ba5e1840c60f9a17d64377e8d8
LOvine <oduor.lovine@gmail.com>
2020-10-17 23:03:46 +0300

    add a string tag in mainactivity

--- app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
@@ -3,12 +3,20 @@ package com.lovineoduor.bloodbankapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+    public static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
