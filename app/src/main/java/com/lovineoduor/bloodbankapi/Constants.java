bd9699102d34675b2c5c8d0bb2d9f229d96ed3a1
LOvine <oduor.lovine@gmail.com>
2020-10-18 21:01:06 +0300

    add an apikey

---
+++ app/src/main/java/com/lovineoduor/bloodbankapi/Constants.java
@@ -0,0 +1,4 @@
+package com.lovineoduor.bloodbankapi;
+
+public class Constants {
+}
--- app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/MainActivity.java
@@ -1,28 +1,47 @@
 package com.lovineoduor.bloodbankapi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
-    public static final String TAG = MainActivity.class.getSimpleName();
-    @BindView(R.id.donation) Button mdonate;
-    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
-    @BindView(R.id.saveddonors) Button mSaveddonors;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lovineoduor.myrestaurants.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.donation)
+    Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-    }
 
-    @Override
-    public void onClick(View v) {
-
+        Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        mAppNameTextView.setTypeface(caviarFont);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, BloodBankActivity.class);
+                String location = mLocationEditText.getText().toString();
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
