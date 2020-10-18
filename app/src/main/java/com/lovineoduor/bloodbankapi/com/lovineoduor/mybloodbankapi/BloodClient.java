252f7977cdc2881e77b6f6987f67d43c1cf729c1
LOvine <oduor.lovine@gmail.com>
2020-10-18 23:34:59 +0300

    add a okhttpclient

--- app/src/main/java/com/lovineoduor/bloodbankapi/com/lovineoduor/mybloodbankapi/BloodClient.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/com/lovineoduor/mybloodbankapi/BloodClient.java
@@ -1,5 +1,19 @@
 package com.lovineoduor.bloodbankapi.com.lovineoduor.mybloodbankapi;
 
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
 public class BloodClient {
+
+    private static Retrofit retrofit = null;
+
+    public static BloodBankApi getClient() {
+
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+
+        }
+
+    }
 
 }
