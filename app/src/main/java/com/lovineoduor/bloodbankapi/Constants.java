9ca2495148f2d174724e34fdf6ac6e0d1d4ea69e
LOvine <oduor.lovine@gmail.com>
2020-10-18 21:04:51 +0300

    add a base url link

--- app/src/main/java/com/lovineoduor/bloodbankapi/Constants.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/Constants.java
@@ -1,4 +1,6 @@
 package com.lovineoduor.bloodbankapi;
 
 public class Constants {
+    public static final String BLOODBANK_BASE_URL = "https://addata.gov.ae/api/action/datastore/search.json?";
+
 }
