40493100b584d3e802a3604a7d5f8411b169c9ce
LOvine <oduor.lovine@gmail.com>
2020-10-18 23:09:51 +0300

    add a donate and request strings

--- app/src/main/java/com/lovineoduor/bloodbankapi/com/lovineoduor/mybloodbankapi/BloodBank.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/com/lovineoduor/mybloodbankapi/BloodBank.java
@@ -1,8 +1,14 @@
 package com.lovineoduor.bloodbankapi.com.lovineoduor.mybloodbankapi;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface BloodBank {
 
-    @GET("")
+    @GET("bloodbank")
+    Call<BloodBank> getBlood(
+            @Query("donate") String donate,
+            @Query("request") String request
+    );
 }
