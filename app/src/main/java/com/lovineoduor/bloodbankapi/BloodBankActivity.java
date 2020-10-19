4f3ad79db20b7cb1c7ff4e399d029f1d91459bb6
LOvine <oduor.lovine@gmail.com>
2020-10-19 17:07:15 +0300

    add a progressview bindview

--- app/src/main/java/com/lovineoduor/bloodbankapi/BloodBankActivity.java
+++ app/src/main/java/com/lovineoduor/bloodbankapi/BloodBankActivity.java
@@ -1,4 +1,21 @@
 package com.lovineoduor.bloodbankapi;
 
-public class BloodBankActivity {
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lovineoduor.bloodbankapi.com.lovineoduor.mybloodbankapi.BloodBank;
+
+import butterknife.BindView;
+
+public class BloodBankActivity<progressBar> extends AppCompatActivity {
+    private static final String TAG = BloodBankActivity.class.getSimpleName();
+
+    @BindView(R.id.donationTextView) TextView mdonationTextView;
+    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.requestView) TextView mrequestTextView;
+    @BindView(R.id.progressBar) progressBar mprogressBar;
+
+
 }
